eb66e2deaff0c1e65385629ea41cabf5bf491198
Eyyub Shiraliyev <148553485+3yyub21@users.noreply.github.com>
2024-02-29 23:01:59 +0100

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int rnd;
+        private int count;
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                count = 0;
+                Random random = new Random();
+                int begin = int.Parse(BeginRange.Text);
+                int end = int.Parse(EndRange.Text);
+                var guess = random.Next(begin, end);
+                rnd = guess;
+            }
+
+            catch (FormatException)
+            {
+                MessageBox.Show("The Format is wrong, Please enter the valid integer.");
+            }
+
+            catch (Exception exception)
+            {
+                MessageBox.Show("Error: " + exception.Message);
+            }
+
+            Text.Text = "Please guess a number and press Guess:";
+        }
+
+        private void Guess_Click(object sender, RoutedEventArgs e)
+        {
+            count++;
+
+            try
+            {
+                int myguess = int.Parse(myGuess.Text);
+
+                if (myguess > rnd)
+                {
+                    Text.Text = $"My number is less than {myguess}";
+                }
+
+                else if (myguess < rnd)
+                {
+                    Text.Text = $"My number is greater than {myguess}";
+                }
+
+                else
+                {
+                    Text.Text = $"Well Done! It took you {count} attempts.";
+                }
+            }
+
+            catch (FormatException)
+            {
+                MessageBox.Show("The Format is wrong, Please enter the valid integer.");
+            }
+
+            catch(Exception exception)
+            {
+                MessageBox.Show("Error: " + exception.Message);
+            }
+        }
+    }
+}
